f1b6a7c2801009b6f6f180e991698c7258d437b2
Zachareee <zacharytanhx@yahoo.com>
2024-01-30 14:26:42 +0800

    Added auto overflow on Objects panel

--- src/EditorPane.js
+++ src/EditorPane.js
@@ -1,10 +1,7 @@
-const EditorPane= (props) => {
-    const className="grid grid-cols-2 auto-rows-min"
-    return (
-        <div className={className}>
-            {props.children}
-        </div>
-    )
+const EditorPane = (props) => {
+    return <>
+        {props.children}
+    </>
 }
 
 export default EditorPane 
--- src/tabs/Objects.js
+++ src/tabs/Objects.js
@@ -3,17 +3,23 @@ import Circle from "../SVG/Circle"
 import Square from "../SVG/Square"
 
 const Objects = () => {
-    return <>
-        <Draggable type="BOX">
-            <Circle x="50" y="50" r="25" style={{ "fill": "purple" }} />
-        </Draggable>
-        <Draggable type="BOX">
-            <Square l="50" x="50" y="50" fill="red" id="red-square" />
-        </Draggable>
-        <Draggable type="BOX">
-            <Circle x="20" y="50" r="50" style={{ "fill": "blue" }} />
-        </Draggable>
-    </>
+    const className = "grid grid-cols-2 auto-rows-min overflow-auto"
+    return (
+        <div className={className}>
+            <Draggable type="BOX">
+                <Circle x="50" y="50" r="25" style={{ "fill": "purple" }} />
+            </Draggable>
+            <Draggable type="BOX">
+                <Square l="50" x="50" y="50" fill="red" id="red-square" />
+            </Draggable>
+            <Draggable type="BOX">
+                <Circle x="20" y="50" r="50" style={{ "fill": "blue" }} />
+            </Draggable>
+            <Draggable type="BOX">
+                <Square l="25"/>
+            </Draggable>
+        </div>
+    )
 }
 
 export default Objects
