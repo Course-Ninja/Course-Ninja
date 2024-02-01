4387c5bb570e69e4a9c7dbba0da9229270d65b77
Zachareee <zacharytanhx@yahoo.com>
2024-02-01 12:08:02 +0800

    Added delete hover effect

--- src/windows/EditorPane.js
+++ src/windows/EditorPane.js
@@ -1,7 +1,19 @@
+import { useDrop } from "react-dnd"
+import Dragtype from "../drags/Dragtype"
+
 const EditorPane = (props) => {
-    return <>
+    const [{ isOver }, drop] = useDrop(() => ({
+        accept: [Dragtype.Moveable],
+        collect: monitor => ({
+            isOver: monitor.isOver()
+        })
+    }))
+    return <div ref={drop} className={`relative resize-x ${isOver ? "bg-red-400" : ""}`}>
+        <div className={`flex absolute w-full h-full justify-center items-center ${isOver ? "" : "hidden"}`}>
+            <p className="text-2xl font-bold">Delete</p>
+        </div>
         {props.children}
-    </>
+    </div>
 }
 
 export default EditorPane 
