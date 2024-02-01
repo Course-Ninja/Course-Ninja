f42f52d32f625421eff272860750df5e216cb874
Zachareee <zacharytanhx@yahoo.com>
2024-02-01 11:07:17 +0800

    Added movement on whiteboard

--- src/Draggable.js
+++ src/Draggable.js
@@ -4,12 +4,13 @@ const Draggable = (props) => {
     const [collected, drag, dragpreview] = useDrag(() => (
         {
             type: props.type,
-            item: { id: props.id, obj: props.children },
+            item: { id: props.dragid, obj: props.children, left: props.left, top: props.top},
             collect: (monitor) => ({
                 isDragging: monitor.isDragging()
             })
         }
     ))
+
     return (
         <div ref={collected.isDragging ? dragpreview : drag} className={props.className} style={props.style}>
             <svg viewBox="0 0 100 100" className="size-full">
--- src/Whiteboard.js
+++ src/Whiteboard.js
@@ -2,29 +2,44 @@ import { createElement, useCallback, useRef, useState } from "react"
 import { useDrop } from "react-dnd"
 import Draggable from "./Draggable"
 import Dragtype from "./Dragtype"
+import { v4 as uuid } from "uuid"
 
 const Whiteboard = (props) => {
     const tileSize = 150 // arbitrary value for center of tile, remove once better method is found
     const className = "rounded-md border-4 border-slate-500 col-span-3 flex items-center justify-center relative"
-    const [elements, setElements] = useState([])
+    const [elements, setElements] = useState({})
     const ref = useRef(null)
 
     const addElement = useCallback(
-        (element, left, top) => {
-            setElements(elems => [...elems, { element, left, top }])
+        ({ id, obj }, left, top) => {
+            setElements(elems => { return { ...elems, [id ? id : uuid()]: { obj, left, top } } })
+        }, [setElements]
+    )
+
+    const moveElement = useCallback(
+        ({ id, obj }, left, top) => {
+            setElements(elems => { return { ...elems, [id]: { obj, left, top } } })
         }, [setElements]
     )
 
     const [, drop] = useDrop(() => ({
         drop: (item, monitor) => {
+            const delta = monitor.getClientOffset()
+            const left = Math.round(delta.x - tileSize)
+            const top = Math.round(delta.y - tileSize)
             switch (monitor.getItemType()) {
                 case Dragtype.MenuTile:
-                    const delta = monitor.getClientOffset()
-                    const left = Math.round(delta.x - tileSize)
-                    const top = Math.round(delta.y - tileSize)
-                    addElement(item.obj, left, top)
+                    // console.log("Create:", item.id)
+                    addElement(item, left, top)
                     return undefined
                 case Dragtype.Moveable:
+                    // const deltamove = monitor.getDifferenceFromInitialOffset()
+                    // const leftmove = Math.round(item.left + deltamove.x)
+                    // const topmove = Math.round(item.top + deltamove.y)
+                    // console.log("Move:", item.id)
+                    // moveElement(item, leftmove, topmove)
+                    moveElement(item, left, top)
+                    return undefined
                 default:
             }
         },
@@ -38,15 +53,19 @@ const Whiteboard = (props) => {
                 ref.current = el
             }
         } className={className} {...props}>
-            {elements.map(
-                ({ element: { type, props }, left, top }, key) => {
+            {Object.entries(elements).map(
+                ([id, { obj: { type, props }, left, top }], key) => {
                     return (
-                        <Draggable key={key} className="fixed" style={{ left, top }} type={Dragtype.Moveable}>
+                        <Draggable dragid={id} // for element movement
+                            key={key} // array map key
+                            className="fixed" style={{ left, top }} // absolute positioning on whiteboard
+                            left={left} top={top} // pass coordinates to Draggable
+                            type={Dragtype.Moveable} /*drag type*/>
                             {createElement(type, { ...props })}
                         </Draggable>
                     )
-                }
-            )}
+                })
+            }
         </div>
     )
 }
