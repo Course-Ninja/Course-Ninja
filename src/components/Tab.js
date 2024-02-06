bfa2e4967410b490f953ab9b73f9fcd9c7666904
Zachareee <zacharytanhx@yahoo.com>
2024-02-06 09:25:24 +0800

    Decluttered, removed wonky useCallbacks

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,14 +1,10 @@
-import { useCallback, useContext } from "react"
+import { useContext } from "react"
 import { TabContext } from "../windows/EditorPane"
 
 const Tab = ({ name, children, style }) => {
     const { setActiveTab } = useContext(TabContext)
 
-    const switchTab = useCallback(() => {
-        setActiveTab(name)
-    }, [setActiveTab, name])
-
-    return <div onClick={switchTab}
+    return <div onClick={() => {setActiveTab(name)}}
         className="aspect-square rounded-l-lg cursor-pointer flex flex-col flex-none justify-center items-center hover:bg-indigo-700 hover:text-white"
         style={style}>
         {children}
