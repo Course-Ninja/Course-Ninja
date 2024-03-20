6736f548d129bd8bec47807ec7348f3d9686999f
Ryan Loh <ryanloh28@yahoo.com.sg>
2024-03-20 12:21:31 +0800

    style: fixed test button div height

--- src/windows/TestRun.js
+++ src/windows/TestRun.js
@@ -9,9 +9,9 @@ const TestRun = () => {
     const stop = () => {
         setTesting(false)
     }
-    return <div className="ml-2 flex h-[6%] w-1/2">
-        <img className="p-1 cursor-pointer" src="play-512.png" alt="start" onClick={start} />
-        <img className="p-1 cursor-pointer" src="hexagon-512.png" alt="stop" onClick={stop} />
+    return <div className="ml-2 flex h-8 w-1/2">
+        <img className="p-1 cursor-pointer h-8" src="play-512.png" alt="start" onClick={start} />
+        <img className="p-1 cursor-pointer h-8" src="hexagon-512.png" alt="stop" onClick={stop} />
     </div>
 }
 
