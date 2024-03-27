85618a20f88bc9bf0de8ceed887a2c13661ec1a3
Zachareee <zacharytanhx@yahoo.com>
2024-03-27 11:26:56 +0800

    Added removeScreen function

--- src/windows/Screens.js
+++ src/windows/Screens.js
@@ -11,17 +11,20 @@ const Screens = () => {
         setActiveScreen(screens.length)
     }
 
+    const removeScreen = idx => {
+        if (screens.length !== 1) {
+            if (activeScreen) setActiveScreen(num => num - 1)
+            setScreens(screens => screens.filter((sc, key) => key !== idx))
+        } else setScreens([{}])
+    }
+
     return <div className="bg-[#BBBBBB] size-full overflow-y-auto">
         {screens.map((element, index) => <div className={`flex items-center justify-center ${activeScreen === index ? "bg-violet-400" : ""}`}
             onClick={() => setActiveScreen(index)}
             key={index}
         >
             <span className="p-2 pl-4 select-none">Screen {index + 1}</span>
-            <div className="aspect-video w-2/5 rounded-md border-4 border-slate-500 m-2 flex flex-grow items-center justify-center relative bg-white">
-                {/* {Object.entries(element).map(([, { id, left, top }]) => <div className="fixed" style={{ left, top }}>
-                    {objRef[id]}
-                </div>)} */}
-            </div>
+            <div className="aspect-video w-2/5 rounded-md border-4 border-slate-500 m-2 flex flex-grow items-center justify-center relative bg-white" />
         </div>)}
         <div className="flex items-center justify-center">
             <span className="p-2 pl-4 invisible">Screen 9</span>
