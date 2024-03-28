3bae9c440c32ea0ee9e02d4bc4a7f4114e8fa858
Ryan Loh <ryanloh28@yahoo.com.sg>
2024-03-28 22:27:41 +0800

    added basket png

--- src/tabs/FruitsTab.js
+++ src/tabs/FruitsTab.js
@@ -4,6 +4,7 @@ const FruitsTab = () => {
     return <TileContainer>
         <img src="https://assets-global.website-files.com/5f778340ed26b167bd087abe/634979ed000ded7d0b9f4f21_apple.png" alt="HI"></img>
         <img src="https://cdn-icons-png.flaticon.com/512/3721/3721818.png" alt=""></img>
+        <img src="./basket.png" alt=""></img>
     </TileContainer>
 }
 
